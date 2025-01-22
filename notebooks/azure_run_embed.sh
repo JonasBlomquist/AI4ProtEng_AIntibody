8937f38e7c073a8ff97385f64cfc3aa82919dcb6
JonathanDragestadM <jonathanmoeller@live.dk>
2025-01-22 18:52:45 +0000

    running stuff

--- notebooks/azure_run_embed.sh
+++ notebooks/azure_run_embed.sh
@@ -4,9 +4,28 @@
 echo hello >> test.txt
 which python >> test.txt
 
-python -u embed_EMS.py --seting True --model 1 --cc  1  --file "anti" --token 1 >> test1.txt & 
-python -u embed_EMS.py --seting True --model 1 --cc  1  --file "anti" --token 2 >> test2.txt &
-python -u embed_EMS.py --seting True --model 1 --cc  1  --file "anti" --token 0 >> test3.txt &
+for model in 0  
+do
+
+>test1.txt
+python -u embed_EMS.py --seting True --model $model --cc  0  --file "anti"  >> test1.txt & 
+
+
+for linker in 0 1 2 
+do
+
+>test1.txt
+python -u embed_EMS.py --seting True --model $model --cc  1  --file "anti" --token $linker >> test1.txt  &
+    
+done
+done
+
+# >test1.txt
+# python -u embed_EMS.py --seting True --model 1 --cc  1  --file "anti" --token 1 >> test1.txt & 
+# >test2.txt
+# python -u embed_EMS.py --seting True --model 1 --cc  1  --file "anti" --token 2 >> test2.txt &
+# >test3.txt
+# python -u embed_EMS.py --seting True --model 1 --cc  0   >> test5.txt &
 
 # "Chain choice. 0: sep, 1: together %(default)", 
 # file "covi" or "anti2
