954b696ac64cb07a10b29875c9f1f6d7d1e0500c
JonathanDragestadM <jonathanmoeller@live.dk>
2025-01-22 15:25:17 +0100

    hpc sciprt

--- notebooks/runner.sh
+++ notebooks/runner.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 ### General options
 ### –- specify queue --# gpu: gpuv100, course :c27666
-#BSUB -q c27666  
+#BSUB -q gpuv100  
 ### -- set the job Name --
 #BSUB -J embed_EMS
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 1
+#BSUB -n 8
 # ### -- Select the resources: 1 gpu in exclusive process mode --
 # #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 0:30
+#BSUB -W 6:00
 # request 5GB of system-memory
-#BSUB -R "rusage[mem=700GB]"
+#BSUB -R "rusage[mem=10GB]"
 ### -- specify that the cores must be on the same host --
 #BSUB -R "span[hosts=1]"
 
